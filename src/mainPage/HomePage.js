d135413a20f7f3b7143b349432e03f590828be57
Sanjita Singha <sanjitasingha100@gmail.com>
2018-06-28 19:47:56 +0530

    home page design

--- src/mainPage/HomePage.js
+++ src/mainPage/HomePage.js
@@ -1,7 +1,57 @@
 import React, { Component } from 'react';
 import Sidebar from 'react-sidebar';
 import Header from '../components/Header';
+import '../css/HomePage.css';
 import { Navbar, NavItem, Nav, NavDropdown, MenuItem, Grid, Row, Col, Button } from 'react-bootstrap';
+
+window.onscroll = function() {scrollFunction()};
+
+  // var header = document.getElementById("homeSideUl");
+  // var btns = document.getElementsByClassName("homeList");
+  // console.log(btns, header);
+  //   for(var i = 0; i < btns.length; i++) {
+  //     console.log('BUTT', btns[i]);
+  //     btns[i].addEventListener("click", function() {
+  //       var current = document.getElementsByClassName("homePageActiveList");
+  //       console.log(current);
+  //       current[0].className = current[0].className.replace(" homePageActiveList", "");
+  //       this.className += " homePageActiveList";
+  //     });
+  //   }
+
+
+function scrollFunction() {
+  var d = document.getElementById("yellowPanel");
+  var a = document.getElementById('alarcate-section');
+  var s = document.getElementById('floatSearch');
+  var y = document.getElementById('yellowPanelInner');
+
+  if(d !== null && a!== null) {
+    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+
+        // d.classList.add('positionFixed');
+        // console.log('Scrolled Up', d);
+        d.style.position = 'fixed';
+        d.style.top = 0;
+        d.style.left = 0;
+        d.style.right = 0;
+        a.style.opacity = 0;
+        s.style.transform = 'translate3d(0,-43px,0)';
+        y.style.marginTop = '20px';
+        y.style.marginBottom = '20px';
+
+    } else {
+      console.log('Scrolled Down', d);
+      d.style.position = 'relative';
+      a.style.opacity = 1;
+      s.style.transform = 'translate3d(0,20px,0)';
+      y.style.marginTop = '40px';
+      y.style.marginBottom = '40px';
+
+    }
+  }
+}
+
 const Arr = [
   {
     name: 'Food item Name',
@@ -47,6 +97,15 @@ const Arr = [
     price: 195.63,
     ordered: 255,
     type: 'MEAL'
+  },
+  {
+    name: 'Food item Name',
+    veg: true,
+    pic: '',
+    description: 'This is description of this food item',
+    price: 195.63,
+    ordered: 255,
+    type: 'MEAL'
   }
 ];
 class HomePage extends Component {
@@ -55,9 +114,24 @@ class HomePage extends Component {
     this.state = {
       sidebarOpen: false
     }
-
+    this._handleClick = this._handleClick.bind(this);
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
   }
+
+  ComponentDidMount() {
+   this._handleClick();
+ }
+
+ _handleClick() {
+   const acc = this._acc.children;
+   for (let i = 0; i < acc.length; i++) {
+     let a = acc[i];
+     console.log('AA', a, acc);
+
+     a.onclick = () => { a.classList.add("homePageActiveList") };
+   }
+ }
+
 
   onSetSidebarOpen(open) {
     this.setState({ sidebarOpen: true });
@@ -75,6 +149,27 @@ class HomePage extends Component {
       return str;
     }
   };
+
+  renderRecommendedList() {
+    return Arr.map(d => {
+      return (
+        <div style={{ width: 250, margin: 20 }}>
+          <img
+            className='listItemImg img-responsive'
+            src={"https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"}  alt="OSK logo" />
+          <p style={{ display: 'inline-block', fontWeight: 'bold', fontSize: 17 }}>{d.name}</p>
+          <span className='vegIndicator' style={{ backgroundColor: d.veg ? 'green' : 'red', marginLeft: 7, marginTop: 25 }}></span>
+
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+
+            <p style={{ marginBottom: 0 }}>Rs. <span style={{ fontWeight: 'bold' }}>{d.price}</span></p>
+
+            <Button className='homeSmallAddCartBtn'>ADD</Button>
+          </div>
+        </div>
+      )
+    })
+  }
 
   render() {
     return (
@@ -120,109 +215,183 @@ class HomePage extends Component {
         </Navbar>
         <div className='bodyContainer'>
           <div>
-            <Grid bsClass='' style={{ backgroundColor: 'orange' }}>
-              <Row className="show-grid">
-                <Col xs={12} md={7} sm={6} className=''>
-                <img
-                  style={{ minWidth: '100%', height: 'auto' }}
-                  className=''
-                   src={require("../assets/images/foodPic.jpg")}  alt="OSK logo" width="300" />
-                </Col>
-                <Col xs={12} md={5} sm={6} className='' style={{ marginTop: 15 }}>
-                  <h5 className='foodName'>Food name</h5>
-                  <p>No. of times ordered</p>
-                  <ul className='listItemPricePanel'>
-                    <li>
-                      <span className='vegIndicator' style={{ backgroundColor: 'veg' ? 'green' : 'red'}}></span>
+            <div style={{ position: 'relative', height: 300 }}>
+              <Grid bsClass='' id='yellowPanel' style={{ backgroundColor: '#FFD500' }}>
+                <Row className="show-grid">
+                  <div className='container' id='yellowPanelInner' style={{ padding: 0, margin: 40, transition: 'all .5s' }}>
+                    <Col xs={12} md={4} sm={6} className='' style={{ position: 'relative' }}>
+                    <img
+                      style={{ minWidth: '100%', height: 'auto' }}
+                      className=''
+                       src={require("../assets/images/foodPic.jpg")}  alt="OSK logo" width="300" />
+                    </Col>
+                    <Col xs={12} md={4} sm={6} className=''>
+                      <h5 className='foodName'>Food name</h5>
+                      <ul className='homePagelistItemPricePanel'>
+                        <li>
+                          <select className='foodQuantitySelection pricePanelBoldText'>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
 
-                    </li>
-                    <li>A-LA-CARTE</li>
-                    <li>Rs. 1256</li>
-                  </ul>
-                  <p>Ingredients</p>
-                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <select name="quantity" >
-                      <option value={1}>1</option>
-                      <option value={2}>2</option>
-                      <option value={3}>3</option>
-                      <option value={4}>4</option>
-                    </select>
-                    <Button bsStyle="danger" className='addCartButton'>ADD TO CART</Button>
+                          </select>
+                          <span className='lightSmallGreyFont'>Quantity</span>
+                        </li>
+                        <li>
+                          <span className='vegIndicator' style={{ backgroundColor: 'veg' ? 'green' : 'red'}}></span>
+                          <span className='lightSmallGreyFont'>{'veg' ? 'Veg': 'Non-veg'}</span>
+
+                        </li>
+                        <li>
+                          <span className='pricePanelBoldText'>Rs. 1256</span>
+                          <span className='lightSmallGreyFont'>Price</span>
+
+                        </li>
+
+                      </ul>
+                      <div className='noOrderedContainer' id='alarcate-section'>
+                        <p className='alacarteText' style={{ backgroundColor: 'red' }}>A-LA-CARTE</p>
+                        <p className='noOrderText' style={{ backgroundColor: 'blue' }}>15 times ordered</p>
+                      </div>
+                      <div className='floatSearchSection' id='floatSearch'>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                          <div className="homePageFoodInput">
+                            <i className='fa fa-search' />
+                            <input type="text"
+                              placeholder="search your favourite food"
+                              onChange={(val) => this.setState({ mobileNum: val.target.value })}
+                            />
+                          </div>
+                          <div className="homecheckboxWhiteBox">
+                            <label class="homePagecheckBoxContainer">Veg
+                            <input type="checkbox"
+
+                            />
+                            <span className='homePageCheckBox'></span>
+                            </label>
+                          </div>
+                          <Button bsStyle="danger" className='homePageAddCartButton'>ADD TO CART</Button>
+                        </div>
+                      </div>
+
+                    </Col>
                   </div>
-
-                </Col>
-              </Row>
-            </Grid>
-            <div style={{ padding: 15 }}>
-              <p>
-                He went such dare good mr fact. The small own seven saved man age ﻿no offer.
-                Suspicion did mrs nor furniture smallness. Scale whole downs often leave not eat.
-                An expression reasonably cultivated indulgence mr he surrounded instrument.
-                Gentleman eat and consisted are pronounce distrusts.
-                Bringing unlocked me an striking ye perceive. Mr by wound hours oh happy.
-                Me in resolution pianoforte continuing we. Most my no spot felt by no.
-                He he in forfeited furniture sweetness he arranging. Me tedious so to behaved written
-                account ferrars moments. Too objection for elsewhere her preferred allowance her.
-                Marianne shutters mr steepest to me.
-                Up mr ignorant produced distance although is sociable blessing.
-                Ham whom call all lain like.
-              </p>
+                </Row>
+              </Grid>
             </div>
 
+            <div className='container'>
+              <div>
+                <h4 className='homePageHeading'>Ingredients-</h4>
+                <p className='homeMainPara'>Pasta, Chicken, Tomato, Garlic, Bell pepper, Mushroom, Cheese, Olive oil</p>
+              </div>
+
+              <div style={{ paddingTop: 15, paddingBottom: 15 }}>
+                <h4 className='homePageHeading'>Description-</h4>
+                <p className='homeMainPara'>
+                  He went such dare good mr fact. The small own seven saved man age ﻿no offer.
+                  Suspicion did mrs nor furniture smallness. Scale whole downs often leave not eat.
+                  An expression reasonably cultivated indulgence mr he surrounded instrument.
+                  Gentleman eat and consisted are pronounce distrusts.
+                  Bringing unlocked me an striking ye perceive. Mr by wound hours oh happy.
+                  Me in resolution pianoforte continuing we. Most my no spot felt by no.
+                  He he in forfeited furniture sweetness he arranging. Me tedious so to behaved written
+                  account ferrars moments. Too objection for elsewhere her preferred allowance her.
+                  Marianne shutters mr steepest to me.
+                  Up mr ignorant produced distance although is sociable blessing.
+                  Ham whom call all lain like.
+                </p>
+              </div>
+
+              <div id="allFood" className="bodyContainer" style={{ display: 'flex' }}>
+                <div style={{ position: 'relative', width: '215px' }} >
+                  <div style={{ height: 0, width: 215, position: 'sticky', top: 185, marginTop: 15 }}>
+                    <ul className="homePageListItemUl" id='homeSideUl' ref={a => this._acc = a}
+        onClick={this._handleClick}>
+                      <li className="homeList homePageActiveList">
+                        <a href='#recommended'><span> Recommended</span></a>
+                      </li>
+                      <li className="homeList">
+                        <a href='#meals'><span> Meals</span></a>
+                      </li>
+                      <li className="homeList">
+                        <a href='#veg'><span> Vegetarian Option</span></a>
+                      </li>
+                      <li className="homeList">
+                        <a href='#nonVeg'><span> Non-Veg</span></a>
+                      </li>
+                      <li className="homeList">
+                        <a href='#alacarte'><span> A-LA-CARTE</span></a>
+                      </li>
+                      <li className="homeList">
+                        <a href='#night'><span> Late Night</span></a>
+                      </li>
+                      <li className="homeList">
+                        <a href='#diet'><span> Diet Meal</span></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div style={{ borderLeft: '1px solid gainsboro', padding: '10px 40px' }}>
+                  <div id='recommended' className='homeDataListMenu'>
+                    <h4 className='homePageHeading'>Recommended Items</h4>
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      {this.renderRecommendedList()}
+                    </div>
+                  </div>
+                  <div id='meals' className='homeDataListMenu'>
+                    <h4 className='homePageHeading'>Meals</h4>
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      {this.renderRecommendedList()}
+                    </div>
+                  </div>
+                  <div id='veg' className='homeDataListMenu'>
+                    <h4 className='homePageHeading'>Vegetarian Option</h4>
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      {this.renderRecommendedList()}
+                    </div>
+                  </div>
+                  <div id='alacarte' className='homeDataListMenu'>
+                    <h4 className='homePageHeading'>A-la-carte</h4>
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      {this.renderRecommendedList()}
+                    </div>
+                  </div>
+                  <div id='night' className='homeDataListMenu'>
+                    <h4 className='homePageHeading'>Late Night Menu</h4>
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      {this.renderRecommendedList()}
+                    </div>
+                  </div>
+                  <div id='diet' className='homeDataListMenu'>
+                    <h4 className='homePageHeading'>Diet Meal</h4>
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      {this.renderRecommendedList()}
+                    </div>
+                  </div>
+                </div>
+                <div style={{ position: 'relative', width: '200px' }} >
+                  <div style={{ height: 0, width: 200, position: 'sticky', top: 185, marginTop: 15 }}>
+                    <div style={{ padding: '20px 0px'}}>
+                      <h1 style={{ marginBottom: 20, opacity: '0.4' }}>Cart Empty</h1>
+                      <img style={{ opacity: '0.7' }} src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_480/Cart_empty_-_menu_2x_ejjkf2" className="_3mSQq" width='220'/>
+                      <p style={{ marginTop: 15, fontSize: 14, color: 'gainsboro'}}>Good food is always cooking! Go ahead, order some yummy items from the menu</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
           </div>
-        </div>
       </div>
+    </div>
     );
   }
 }
-
-const sidebarStyles = {
-  root: {
-    position: 'absolute',
-    top: 60,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflow: 'hidden',
-  },
-  sidebar: {
-    zIndex: 2,
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    transition: 'transform .3s ease-out',
-    WebkitTransition: '-webkit-transform .3s ease-out',
-    willChange: 'transform',
-    overflowY: 'auto',
-  },
-  content: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflowY: 'scroll',
-    WebkitOverflowScrolling: 'touch',
-    transition: 'left .3s ease-out, right .3s ease-out',
-  },
-  overlay: {
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    opacity: 0,
-    visibility: 'hidden',
-    transition: 'opacity .3s ease-out, visibility .3s ease-out',
-    backgroundColor: 'rgba(0,0,0,.3)',
-  },
-  dragHandle: {
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-  },
-};
 
 export default HomePage;
